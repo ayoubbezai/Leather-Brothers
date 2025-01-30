9c3aee33b3bd23908ff6904b5e0556269f1b5352
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-30 23:06:25 +0100

    secend

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import NavBar from './components/Nav';
+import Hero from './components/Hero';
+import AboutUs from './components/AboutUs';
+import Unique from './components/Unique';
+import Combination from './components/Combination';
+import Video from './components/Video';
+import Sells from './components/Sells';
+import Custmores from './components/Custmores';
+import Blog from './components/Blog';
+import News from './components/News';
+
+function App() {
+  return (
+    <div className='bg-[#FFFBF0] p-0 m-0' >
+
+      <NavBar />
+      <Hero />
+      <AboutUs />
+      <Unique />
+      <Combination />
+      <Video />
+      <Sells />
+      <Custmores />
+      <Blog />
+      <News />
+
+    </div>
+  );
+}
+
+export default App;
